99f0ba783a847c5d00e307bf6aabf3fc9c4b421b
landtry <landry@boldist.co>
2023-04-24 04:03:05 -0500

    Update parser in default template

--- src/templates/DefaultTemplate.jsx
+++ src/templates/DefaultTemplate.jsx
@@ -16,7 +16,7 @@ export default function DefaultTemplate(props) {
       <Head>{parse(seo?.fullHead)}</Head>
       <Header />
       <ArticleLayout meta={seo} {...props}>
-        {parse(content)}
+        {parse(content.toString())}
       </ArticleLayout>
       <Footer />
     </>
